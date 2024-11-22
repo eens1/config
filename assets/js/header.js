b0cc239c079bc579e22333651273cfef7cd0ebcd
eens <eens@localhost.my.domain>
2024-11-22 06:33:41 +0100

    update

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,18 +1,9 @@
-
 document.addEventListener('DOMContentLoaded', () => {
   const hamburger = document.querySelector('.hamburger');
   const navMenu = document.querySelector('.nav-menu');
-  const mainContent = document.querySelector('.main-content');
 
   hamburger.addEventListener('click', () => {
-    const isActive = navMenu.classList.toggle('active');
+    navMenu.classList.toggle('active');
     hamburger.classList.toggle('active');
-
-    // Adjust the main content margin dynamically
-    if (isActive) {
-      mainContent.style.marginTop = `${navMenu.offsetHeight}px`;
-    } else {
-      mainContent.style.marginTop = '0';
-    }
   });
 });
